060bdc8af646b2b0f1f4ec27c47ad30f39695bf3
Badsha012 <badshasheikh361@gmail.com>
2026-03-03 16:29:39 +0600

    added a new file

---
+++ LaLigaPlayer.java
@@ -0,0 +1,22 @@
+class LaLigaPlayer extends FootballPlayer {
+    private int matchesPlayed;
+    private String league = "LaLiga";
+
+    public LaLigaPlayer(String name, int goals, double monthlySalary, int matchesPlayed) {
+        super(name, goals, monthlySalary);
+        this.matchesPlayed = matchesPlayed;
+    }
+
+    @Override
+    public double calculateGoalRate() {
+        return (double) getGoals() / matchesPlayed;
+    }
+
+    public void displayDetails() {
+        System.out.println("Player: " + getName());
+        System.out.println("League: " + league);
+        System.out.println("Goals: " + getGoals());
+        System.out.printf("Goal Rate: %.2f\n", calculateGoalRate());
+        System.out.println("---------------------------");
+    }
+}
